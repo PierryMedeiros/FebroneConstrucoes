4e459b873f88a6544533df0e0e1799ce9e48f5ad
PierryMedeiros <pierrymedeiros11@gmail.com>
2022-06-16 09:00:48 -0300

    Initial commit

---
+++ js/scripts.js
@@ -0,0 +1,86 @@
+
+// Animação do martelo girando
+var ang = 0;
+var hammer = document.getElementById("martelo");
+var anima;
+
+function gira(){
+    hammer.style.transform="rotate(" + ang + "deg)";
+    anima = requestAnimationFrame(gira)
+    if(ang>360){
+        ang=0
+    }
+    ang++
+}
+gira()
+//Fim da animação do martelo
+
+
+//botão whatsapp
+const url = "https://api.whatsapp.com/send?phone=5521996947880&text=Ol%C3%A1%2C%20gostaria%20de%20fazer%20um%20or%C3%A7amento.";
+const btn = document.querySelector("#whats");
+var widthW = 100;
+var btnRotate = 0;
+var animationStart;
+var animationStop;
+
+function openInNewTab(url) {
+    const win = window.open(url, "_blank");
+    win.focus()
+}
+
+
+
+setInterval(callAttention, 5000);
+function callAttention(){
+    animationButton()
+    setTimeout(cancelAnimationButton, 1200)  
+}
+
+function animationButton(){
+    btn.style.width = widthW + "px";
+    btn.style.transform="rotate(" + btnRotate + "deg)";
+
+    if(widthW < 130){
+        widthW+=4;
+    }else if(widthW >= 120 && btnRotate < 360){
+        btnRotate+=7;
+    }
+    animationStart = requestAnimationFrame(animationButton);
+}
+
+function cancelAnimationButton(){
+    cancelAnimationFrame(animationStart)
+    btn.style.width = widthW + "px";
+    btn.style.transform="rotate(" + btnRotate + "deg)";
+    btnRotate = 0;
+
+    if(widthW > 100){
+        widthW-=4;
+    }
+    animationStop = requestAnimationFrame(cancelAnimationButton);
+    if(widthW <= 100){
+        cancelAnimationFrame(animationStop);
+    }
+}
+
+
+btn.addEventListener("click", function(){
+    openInNewTab(url);
+})
+btn.addEventListener("mouseover", animationButton)
+btn.addEventListener("mouseout", cancelAnimationButton)
+//Fim do botão
+
+//Animação portifolio
+
+var portifolio = document.querySelectorAll(".portf");
+var servicosH2 = document.querySelectorAll(".servicos");
+var descricao = document.querySelectorAll(".description")
+
+//portifolio[0].addEventListener("mouseover", function(){
+   // servicosH2[0].style.color="#333"
+    
+//})
+//git branch -M main
+//git push -u origin main
